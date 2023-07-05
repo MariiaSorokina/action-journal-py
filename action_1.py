e7dc1c890e5a4255433ff169a359ac15a2df26e6
msorokina <m.sorokina0702@gmail.com>
2023-07-05 15:10:39 +0400

    two loop solution(in progress bc not perfect)

--- action_1.py
+++ action_1.py
@@ -1,35 +1,33 @@
+ #функция принимает аргумент(список с элементами) возвращает список уникальных элементов
+def unique_users(list_name):
+    unique = set()
+    for user_name in list_name:
+        unique.add(user_name)
+    return list(unique)
+
+
 with open('records', 'r') as journal:
     user_actions_map = {}  # user -> [action1, action2, ...]
     lines = journal.readlines()
     users = []
     actions = []
 
-
-    # функция принимает аргумент(список с элементами) возвращает список уникальных элементов
-    def unique_users(list_name):  # TODO: use hashset (set)
-        unique = []
-        for user_name in list_name:
-            if user_name in unique:
-                continue
-            else:
-                unique.append(user_name)
-        return unique
-
     for line in lines:
-        name_and_log = line.split('] ')[1]  # Eve deleted the folder "Important Documents."
-        num_of_first_space = name_and_log.find(" ")
-        name = name_and_log[0:num_of_first_space]
+        name_and_log = line.split('] ')
+        num_of_first_space = name_and_log[1].find(' ')
+        name = name_and_log[1][:num_of_first_space]
         users.append(name)
+        act = name_and_log[1][num_of_first_space + 1:-2]
+        actions.append(act)
 
-        # TODO вынести на верхний уровень, отрефакторить названия переменных/аргументов
-        unique_users_name = unique_users(users)
-        actions = name_and_log[num_of_first_space:-2]
+    unique_users_name = unique_users(users)
 
-        if name in user_actions_map:
-            user_actions_map[name].append(actions)
-        else:
-            user_actions_map[name] = [actions]
+    for name in user_actions_map:
+        user_actions_map[name].append(act)
+    else:
+        user_actions_map[name] = [act]
 
-    for unique_users_name, action_list in user_actions_map.items():
-        formatted_actions = ',\n '.join(action_list)
-        print(f"{unique_users_name}: {formatted_actions}")
+for unique_user_name in unique_users_name:
+    action_list = user_actions_map.get(unique_user_name, [])
+    formatted_actions = ',\n '.join(action_list)
+    print(f"{unique_user_name}: {formatted_actions}")
