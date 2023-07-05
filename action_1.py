88d321c835298042d5531b13c08bfc693ad0cb1b
msorokina <m.sorokina0702@gmail.com>
2023-07-05 16:44:27 +0400

    ready solution O(n)

--- action_1.py
+++ action_1.py
@@ -1,12 +1,15 @@
- #функция принимает аргумент(список с элементами) возвращает список уникальных элементов
+# функция принимает аргумент(список с элементами) возвращает список уникальных элементов
+import time
+
 def unique_users(list_name):
     unique = set()
     for user_name in list_name:
         unique.add(user_name)
     return list(unique)
 
+start_time = time.time()
 
-with open('records', 'r') as journal:
+with open('pon.txt', 'r') as journal:
     user_actions_map = {}  # user -> [action1, action2, ...]
     lines = journal.readlines()
     users = []
@@ -20,14 +23,25 @@ with open('records', 'r') as journal:
         act = name_and_log[1][num_of_first_space + 1:-2]
         actions.append(act)
 
+        if name in user_actions_map:
+            user_actions_map[name].append(act)
+        else:
+            user_actions_map[name] = [act]
+
     unique_users_name = unique_users(users)
 
-    for name in user_actions_map:
-        user_actions_map[name].append(act)
-    else:
-        user_actions_map[name] = [act]
+    for user_name in unique_users_name:
+        action_list = user_actions_map.get(user_name, [])
+        formatted_actions = ""
+        for i, action in enumerate(action_list, start=1):
+            if i == 1:
+                formatted_actions += f" {i}. {action}\n"
+            else:
+                formatted_actions += f" {i}. {action}\n"
+        print(f"{user_name}:\n{formatted_actions}")
 
-for unique_user_name in unique_users_name:
-    action_list = user_actions_map.get(unique_user_name, [])
-    formatted_actions = ',\n '.join(action_list)
-    print(f"{unique_user_name}: {formatted_actions}")
+end_time = time.time()
+
+execution_time = end_time - start_time
+
+print(f"Execution time: {execution_time} seconds")
