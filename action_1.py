4cc5f9ac5d358cdf336b6646ba46e88fa84e3ec5
msorokina <m.sorokina0702@gmail.com>
2023-07-07 14:33:58 +0400

    ready solution O(n) with hashmap

--- action_1.py
+++ action_1.py
@@ -1,44 +1,27 @@
 # функция принимает аргумент(список с элементами) возвращает список уникальных элементов
 import time
 
-def unique_users(list_name):
-    unique = set()
-    for user_name in list_name:
-        unique.add(user_name)
-    return list(unique)
-
 start_time = time.time()
 
 with open('pon.txt', 'r') as journal:
     user_actions_map = {}  # user -> [action1, action2, ...]
     lines = journal.readlines()
-    users = []
-    actions = []
 
     for line in lines:
-        name_and_log = line.split('] ')
-        num_of_first_space = name_and_log[1].find(' ')
-        name = name_and_log[1][:num_of_first_space]
-        users.append(name)
-        act = name_and_log[1][num_of_first_space + 1:-2]
-        actions.append(act)
+        name_start = line.find('] ') + 2
+        name_end = line.find(' ', name_start)
+        name = line[name_start:name_end]
+        action = line[name_end + 1:-2]
 
         if name in user_actions_map:
-            user_actions_map[name].append(act)
+            user_actions_map[name].append(action)
         else:
-            user_actions_map[name] = [act]
+            user_actions_map[name] = [action]
 
-    unique_users_name = unique_users(users)
-
-    for user_name in unique_users_name:
-        action_list = user_actions_map.get(user_name, [])
-        formatted_actions = ""
-        for i, action in enumerate(action_list, start=1):
-            if i == 1:
-                formatted_actions += f" {i}. {action}\n"
-            else:
-                formatted_actions += f" {i}. {action}\n"
-        print(f"{user_name}:\n{formatted_actions}")
+    for name, actions in user_actions_map.items():
+        print(f'{name}:')
+        for i, action in enumerate(actions, 1):
+            print(f' {i}. {action}')
 
 end_time = time.time()
 
